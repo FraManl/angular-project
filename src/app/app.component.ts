a6ecf0b040e32ff5800706146a84368b2bc35a14
Francois Manlay <francois.manlay@gmail.com>
2022-10-23 21:39:28 +0200

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  styles: [
+    `
+      h1 {
+        color: grey;
+      }
+    `,
+  ], // will override app.component.css; should one, either styleUrls or styles, same goes for templateUrls or template
 })
 export class AppComponent {
   title = 'Francois Application';
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-server',
+  selector: '[app-servers]',
   templateUrl: './server.component.html',
+  //   template: '<app-server></app-server>', // either templateUrl or template, one of the two has to be present - can be served inline
 })
 export class ServerComponent {}
