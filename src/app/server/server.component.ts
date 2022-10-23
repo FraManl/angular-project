443b19fe409e053d4987556854ae1177618dea44
Francois Manlay <francois.manlay@gmail.com>
2022-10-23 21:46:01 +0200

    testing

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '[app-servers]',
+  selector: 'app-server',
+  // selector: '[app-servers]', // Select by attribute
+  // selector: '.app-servers', // Select by class
   templateUrl: './server.component.html',
   //   template: '<app-server></app-server>', // either templateUrl or template, one of the two has to be present - can be served inline
 })
