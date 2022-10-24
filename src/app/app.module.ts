6ea74fa9253db446143d8a98928fc21855b971f5
Francois Manlay <francois.manlay@gmail.com>
2022-10-24 20:36:40 +0200

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './successAlert/SuccessAlert.component';
+import { WarningComponent } from './warningAlert/WarningAlert.component';
 
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessComponent,
+    WarningComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/successAlert/SuccessAlert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-successalert',
+  // selector: '[app-servers]', // Select by attribute
+  // selector: '.app-servers', // Select by class
+  templateUrl: './SuccessAlert.component.html',
+  styleUrls: ['./SuccessAlert.component.css'],
+})
+export class SuccessComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
