d47d0c91b10edebfe23a49571f95fc37f972cf54
Francois Manlay <francois.manlay@gmail.com>
2022-10-25 22:46:40 +0200

    testing

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'testServer';
+  userName = '';
+  // resetButton = false;
 
   constructor() {
     setTimeout(() => {
@@ -20,11 +22,20 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
   }
 
   onUpdateServerName(event: any) {
     // this.serverName = event.target.value;
     this.serverName = (<HTMLInputElement>event.target).value; // the same but for typescript, stricter
+  }
+
+  // optional... use property binding directly in html template.. avoid writing this and declaring resetButton = false at the beginning...
+  // onAssignInput(event: any) {
+  //   event.target.value ? (this.resetButton = true) : (this.resetButton = false);
+  // }
+  resetInput() {
+    this.userName = '';
+    // this.resetButton = false;
   }
 }
